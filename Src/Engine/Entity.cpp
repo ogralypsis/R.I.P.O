cca2ddf3c3ded7b0b006299e1f564edad56459b2
Lucia Carrion <luciacarr_97@hotmail.com>
2019-03-09 12:26:29 +0100

    Entity class done
    The first draft of Entity class, i had to create the class "Component" as well to work with it

--- Src/Engine/Entity.cpp
+++ Src/Engine/Entity.cpp
@@ -1,12 +1,52 @@
 #include "Entity.h"
 
-
-
-Entity::Entity()
+Entity::Entity(std::string id)
 {
+	_id = id;
 }
 
-
-Entity::~Entity()
+Entity::~Entity() 
 {
+	// delete each pointer
+	for (std::vector<Component *>::iterator it = _components.begin(); it != _components.end(); ++it)
+		delete (*it);
+	// clear the vector
+	_components.clear();
+}
+
+void Entity::AddComponent(Component * c)
+{
+	// push a new component into the vector
+	_components.emplace_back(c);
+}
+
+void Entity::DeleteComponent(Component * c)
+{
+	int i = 0;
+	bool found = false;
+	// search for the component c
+	while (!found && i < _components.size()) 
+	{
+		// when it's found,
+		if (_components.at(i) == c) 
+		{
+			Component* aux = _components.at(i);
+			// delete the reference from the vector
+			_components.erase(_components.begin() + i);
+			// delete the pointer
+			delete aux;
+			// update flag
+			found = true;
+		}
+		else
+		// keep searching
+			i++;
+	}
+}
+
+void Entity::Update() 
+{
+	// update each component of the entity
+	for (int i = 0; i < _components.size(); i++)
+		_components.at(i)->Update();
 }
--- Src/Engine/Entity.h
+++ Src/Engine/Entity.h
@@ -1,8 +1,22 @@
-#pragma once
+#ifndef ENTITY_H
+#define ENTITY_H
+
+#include "Component.h"
+#include <vector>
+
 class Entity
 {
 public:
-	Entity();
+	Entity(std::string id);
 	~Entity();
-};
 
+	void AddComponent(Component* c);
+	void DeleteComponent(Component* c);
+
+	void Update();
+
+protected:
+	std::vector<Component*> _components;
+	std::string _id;
+};
+#endif
